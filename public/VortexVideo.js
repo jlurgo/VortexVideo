ded4a6e4d7e29f2e8096960deec1fa53ce5041a9
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2013-07-25 14:54:39 -0300

    bla

--- public/VortexVideo.js
+++ public/VortexVideo.js
@@ -1,7 +1,7 @@
 $(function () { 
     var router =  new NodoRouter("principal"); 
             
-    var clienteHTTP = new NodoClienteHTTP('http://kfgodel.info:62626/vortex', 100);             
+    var clienteHTTP = new NodoClienteHTTP('http://kfgodel.info:62626/vortex', 1000);             
     router.conectarBidireccionalmenteCon(clienteHTTP);
     
     var nodo_app_vxv = new NodoAppVortexVideo();        
