c009208e587611e28256bf55d4a7c877fa896c7e
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2013-07-25 06:27:06 -0300

    para subir a heroku

---
+++ app/VortexVideo/VortexVideo.js
@@ -0,0 +1,11 @@
+$(function () { 
+    var router =  new NodoRouter("principal"); 
+            
+    var clienteHTTP = new NodoClienteHTTP('http://kfgodel.info:62626/vortex', 100);             
+    router.conectarBidireccionalmenteCon(clienteHTTP);
+    
+    var nodo_app_vxv = new NodoAppVortexVideo();        
+    router.conectarBidireccionalmenteCon(nodo_app_vxv);   
+    
+    nodo_app_vxv.dibujarEn($('#panel_principal'))
+});
