09c23377c3871af2aef4ee26dbc5980cdca9c444
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2013-08-14 06:40:10 -0300

    13_dibujar_listo

--- NodoTransmisorVideo.js
+++ NodoTransmisorVideo.js
@@ -9,10 +9,8 @@ NodoTransmisorVideo.prototype.start = function(){
     
     this.lbl_nombre_usuario = this.ui.find("#lbl_nombre_usuario");
     this.lbl_nombre_usuario.text(this.o.nombreUsuario);
-    //    var canvas = document.getElementById("canvas");
-//	var	context = canvas.getContext("2d");
-    this.video = this.ui.find("#video")[0];
-    var videoObj = { "video": true };
+    
+    this.video = $('<video id="video" width="320px" height="240px" autoplay></video>')[0];
     var errBack = function(error) {
 			console.log("Video capture error: ", error.code); 
 		};
@@ -20,32 +18,77 @@ NodoTransmisorVideo.prototype.start = function(){
     var _this = this;
 	// Put video listeners into place
 	if(navigator.getUserMedia) { // Standard
-		navigator.getUserMedia(videoObj, function(stream) {
+		navigator.getUserMedia(
+            { "video": true }, 
+            function(stream) {
 			_this.video.src = stream;
 			_this.video.play();
+            _this.setupCanvas();
 		}, errBack);
 	} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-		navigator.webkitGetUserMedia(videoObj, function(stream){
+		navigator.webkitGetUserMedia(
+            { "video": true }, 
+            function(stream){
 			_this.video.src = window.webkitURL.createObjectURL(stream);
 			_this.video.play();
+            _this.setupCanvas();
 		}, errBack);
-	}
+    }        
     
-    $(this.video).click(function(){
+    this.ui.find("#btn_camara").click(function(){
         _this.enviarFrame();
-    });
+    });    
     
     this.portal.pedirMensajes(new FiltroAND([new FiltroXClaveValor("tipoDeMensaje", "vortex.video.pedidoDeFrame"),
                                              new FiltroXClaveValor("usuarioTransmisor", this.o.nombreUsuario)]),
                                             this.pedidoDeFrameRecibido.bind(this));
 };
 
+NodoTransmisorVideo.prototype.setupCanvas = function(){
+    var _this = this;
+    setInterval(function(){
+        _this.muestrearVideo();
+    },100);
+    
+    this.canvas = this.ui.find("#canvas_transmisor");
+    this.canvas[0].width  = 320;
+    this.canvas[0].height = 240;
+    this.canvas.context = this.canvas[0].getContext('2d');
+    
+    this.canvas_dibujo = this.ui.find('#canvas_dibujo');
+    this.canvas_dibujo[0].width  = 320;
+    this.canvas_dibujo[0].height = 240;
+    this.canvas_dibujo.context = this.canvas_dibujo[0].getContext('2d');
+    
+    this.canvas_dibujo.mousedown(function(e){
+		_this.canvas_dibujo.context.moveTo(e.offsetX, e.offsetY);
+		_this.canvas_dibujo.context.beginPath();
+		_this.mouseDown = true;
+    });
+    this.canvas_dibujo.mousemove(function(e){
+		if(_this.mouseDown){			
+			_this.canvas_dibujo.context.lineTo(e.offsetX, e.offsetY);
+			_this.canvas_dibujo.context.stroke();	
+		}
+    });
+    this.canvas_dibujo.mouseup(function(e){
+		_this.mouseDown = false;
+    });
+};
+
+NodoTransmisorVideo.prototype.muestrearVideo = function(){
+    this.canvas.context.drawImage(this.video, 0, 0, 320, 240);
+};
+
 NodoTransmisorVideo.prototype.enviarFrame = function(){
     var canvas = $('<canvas>')[0];
-    canvas.width  = this.video.videoWidth;
-    canvas.height = this.video.videoHeight;
+    canvas.width  = 320;
+    canvas.height = 240;
     var ctx = canvas.getContext('2d');
-    ctx.drawImage(this.video, 0, 0);
+    
+    ctx.drawImage(this.canvas[0], 0, 0);
+    ctx.drawImage(this.canvas_dibujo[0], 0, 0);
+    
     var imagen_serializada = canvas.toDataURL('image/jpeg');
     this.portal.enviarMensaje({
         tipoDeMensaje: "vortex.video.frame",
